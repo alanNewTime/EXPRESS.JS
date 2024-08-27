8e1104111aa562441f618b8aec8af02e824e2109
alanNewTime <alan.omam@newtimegroup.it>
2024-08-27 09:15:50 +0200

    aggiunto commento

--- server/src/index.mjs
+++ server/src/index.mjs
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 3000
 // qui andiamo dentro un file "env" dove ci aspettiamo di trovare una variabile chiamata "PORT" con un valore,
 // questo valore lo prendiamo me lo salviamo dentro una variabile "PORT" qui, e se il file env è vuoto, passiamo 3000 come valore al port
 
+//equivalente del mio database,qui salvo i miei users
 const mockUsers = [
     {id: 1, username:'andy', displayname:'Andy'},
     {id: 2, username:'alan', displayname:'Alan'},
